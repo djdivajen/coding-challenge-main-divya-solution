5741d12018ef84ba2713571b4e4cf6d3197ffa05
Divya Jain 8 <divjain2@WKMZT9CF6308.global.publicisgroupe.net>
2021-11-30 17:15:55 +0530

    test case:changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
+import React from react
 import {render, screen} from '@testing-library/react'
 import App from './App'
 
 test('renders landing page UI', () => {
-  // eslint-disable-next-line react/react-in-jsx-scope
   render(<App />)
   const navLinkELement = screen.getByText(/Page B/i)
   expect(navLinkELement).toBeInTheDocument()
